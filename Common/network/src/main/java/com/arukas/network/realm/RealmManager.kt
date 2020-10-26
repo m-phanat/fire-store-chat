edaa45b134ed83298c7abbdbf0172e5b353b6b64
Nut <>
2020-10-26 16:26:38 +0700

    add function get member in room

--- Common/network/src/main/java/com/arukas/network/realm/RealmManager.kt
+++ Common/network/src/main/java/com/arukas/network/realm/RealmManager.kt
@@ -109,4 +109,8 @@ class RealmManager(private val context: Context) {
     fun getAllMessage(chatId: String):Flowable<RealmResults<Message>>? {
         return realm?.where(Message::class.java)?.equalTo("chatId",chatId)?.findAll()?.asFlowable()
     }
+
+    fun getMemberInRoom(chatId:String,myUserId:String):Flowable<RealmResults<Member>>?{
+        return realm?.where(Member::class.java)?.equalTo("chatId",chatId)?.notEqualTo("userId",myUserId)?.findAll()?.asFlowable()
+    }
 }
